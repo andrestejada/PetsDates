c6af198a0d218a50c0e34f203358b810d9651d82
Andrés Eduardo Tejada - Ceiba Software <andres.tejada@ceiba.com.co>
2021-11-30 10:33:42 -0500

    duplication code smell

---
+++ src/app/shared/fixture/dates.ts
@@ -0,0 +1,9 @@
+import { Cita } from '../../feature/Citas/interfaces/index';
+export const newDate:Cita ={
+    nombrePropietario: 'Juanito',
+    nombreMascota: 'mateo',
+    tipoServicio: 'basico',
+    tarifa: 10000,
+    fechaHora: '2021-12-01T16:00',
+    observaciones: 'pelo corto',
+  };
